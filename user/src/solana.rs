52bbe32a18f5e0ea90836ab4598268b77feb667c
doradelta <64730977+doradelta@users.noreply.github.com>
2024-11-12 13:39:23 +0100

    Update solana.rs

--- user/src/solana.rs
+++ user/src/solana.rs
@@ -18,19 +18,21 @@ use {
     solana_client::nonblocking::rpc_client::RpcClient,
     solana_sdk::{hash::Hash, transaction::Transaction},
 };
+use tokio::time::Duration;
+use tokio::time::sleep;
 
 pub fn escrow_and_store_intent_solana(
     src_user: &Rc<Keypair>,
     auctioneer_state: Pubkey,
     client: &Client<Rc<Keypair>>,
-    intent_id: String,     // Pass in the intent ID
-    amount_in: u64,        // Pass in the input amount
-    token_in: Pubkey,      // Pass in the input token
-    dst_user: Pubkey,      // Pass in the destination user
-    token_out: String,     // Pass in the output token
-    amount_out: String,    // Pass in the output amount
-    timeout_duration: u64, // Pass in the timeout duration
-    single_domain: bool,   // Pass in the single domain flag
+    intent_id: String,
+    amount_in: u64,
+    token_in: Pubkey,
+    dst_user: Pubkey,
+    token_out: String,
+    amount_out: String,
+    timeout_duration: u64,
+    single_domain: bool,
 ) -> Result<Signature, String> {
     let program = match client.program(bridge_escrow::ID) {
         Ok(prog) => prog,
@@ -43,7 +45,6 @@ pub fn escrow_and_store_intent_solana(
     let intent_state =
         Pubkey::find_program_address(&[b"intent", intent_id.as_bytes()], &bridge_escrow::ID).0;
 
-    // Get the current timestamp
     let current_timestamp = SystemTime::now()
         .duration_since(UNIX_EPOCH)
         .unwrap()
@@ -51,43 +52,48 @@ pub fn escrow_and_store_intent_solana(
 
     println!("intent_id: {intent_id}");
 
-    // Define the new intent payload
     let new_intent = bridge_escrow::IntentPayload {
         intent_id: intent_id.clone(),
-        user_in: src_user.pubkey(), // Must match the ctx.accounts.user key in the contract
+        user_in: src_user.pubkey(),
         user_out: dst_user.to_string(),
         token_in: token_in,
         amount_in: amount_in,
         token_out: token_out,
-        amount_out: amount_out, // Amount out as a string
+        amount_out: amount_out,
         timeout_timestamp_in_sec: current_timestamp + timeout_duration,
         single_domain: single_domain,
     };
 
-    let sig = program
-        .request()
-        .accounts(bridge_escrow::accounts::EscrowAndStoreIntent {
-            user: src_user.pubkey(),
-            user_token_account: user_token_in_addr,
-            auctioneer_state,
-            token_mint: token_in,
-            escrow_token_account: token_in_escrow_addr,
-            intent: intent_state,
-            token_program: anchor_spl::token::ID,
-            associated_token_program: associated_token::ID,
-            system_program: system_program::ID,
-        })
-        .args(bridge_escrow::instruction::EscrowAndStoreIntent { new_intent })
-        .payer(src_user.clone())
-        .signer(&*src_user)
-        .send_with_spinner_and_config(RpcSendTransactionConfig {
-            skip_preflight: true,
-            ..Default::default()
-        });
+    loop {
+        let sig = program
+            .request()
+            .accounts(bridge_escrow::accounts::EscrowAndStoreIntent {
+                user: src_user.pubkey(),
+                user_token_account: user_token_in_addr,
+                auctioneer_state,
+                token_mint: token_in,
+                escrow_token_account: token_in_escrow_addr,
+                intent: intent_state,
+                token_program: anchor_spl::token::ID,
+                associated_token_program: associated_token::ID,
+                system_program: system_program::ID,
+            })
+            .args(bridge_escrow::instruction::EscrowAndStoreIntent { new_intent: new_intent.clone() })
+            .payer(src_user.clone())
+            .signer(&*src_user)
+            .send_with_spinner_and_config(RpcSendTransactionConfig {
+                skip_preflight: true,
+                ..Default::default()
+            });
 
-    match sig {
-        Ok(signature) => Ok(signature),
-        Err(err) => Err(format!("Transaction failed: {}", err)),
+        match sig {
+            Ok(signature) => break Ok(signature), // Transaction succeeded, exit loop
+            Err(err) if err.to_string().contains("unable to confirm transaction") => {
+                eprintln!("Transaction failed: {}. Retrying...", err);
+                let _= sleep(Duration::from_secs(1));
+            },
+            Err(err) => break Err(format!("Transaction failed due to a non-retryable error: {}", err)), // Break on other errors
+        }
     }
 }
 
@@ -95,26 +101,26 @@ pub fn escrow_and_store_intent_cross_chain_solana(
     src_user: &Rc<Keypair>,
     auctioneer_state: Pubkey,
     client: &Client<Rc<Keypair>>,
-    intent_id: String,     // Pass in the intent ID
-    amount_in: u64,        // Pass in the input amount
-    token_in: Pubkey,      // Pass in the input token
-    dst_user: String,      // Pass in the destination user (cross-chain)
-    token_out: String,     // Pass in the output token (cross-chain)
-    amount_out: String,    // Pass in the output amount (cross-chain)
-    timeout_duration: u64, // Pass in the timeout duration
-    single_domain: bool,   // Pass in the single domain flag
+    intent_id: String,
+    amount_in: u64,
+    token_in: Pubkey,
+    dst_user: String,
+    token_out: String,
+    amount_out: String,
+    timeout_duration: u64,
+    single_domain: bool,
 ) -> Result<Signature, String> {
     let program = match client.program(bridge_escrow::ID) {
         Ok(prog) => prog,
         Err(err) => return Err(format!("Failed to get program: {}", err)),
     };
+
     let user_token_in_addr = get_associated_token_address(&src_user.pubkey(), &token_in);
     let token_in_escrow_addr = get_associated_token_address(&auctioneer_state, &token_in);
 
     let intent_state =
         Pubkey::find_program_address(&[b"intent", intent_id.as_bytes()], &bridge_escrow::ID).0;
 
-    // Get the current timestamp
     let current_timestamp = SystemTime::now()
         .duration_since(UNIX_EPOCH)
         .unwrap()
@@ -122,45 +128,48 @@ pub fn escrow_and_store_intent_cross_chain_solana(
 
     println!("intent_id: {intent_id}");
 
-    // Define the new intent payload
     let new_intent = bridge_escrow::IntentPayload {
         intent_id: intent_id.clone(),
-        user_in: src_user.pubkey(), // Must match the ctx.accounts.user key in the contract
+        user_in: src_user.pubkey(),
         user_out: dst_user,
         token_in: token_in,
         amount_in: amount_in,
         token_out: token_out,
-        amount_out: amount_out, // Amount out as a string
+        amount_out: amount_out,
         timeout_timestamp_in_sec: current_timestamp + timeout_duration,
         single_domain: single_domain,
     };
 
-    let sig = program
-        .request()
-        // .instruction(ComputeBudgetInstruction::set_compute_unit_limit(1_000_000))
-        // .instruction(ComputeBudgetInstruction::request_heap_frame(128 * 1024))
-        .accounts(bridge_escrow::accounts::EscrowAndStoreIntent {
-            user: src_user.pubkey(),
-            user_token_account: user_token_in_addr,
-            auctioneer_state,
-            token_mint: token_in,
-            escrow_token_account: token_in_escrow_addr,
-            intent: intent_state,
-            token_program: anchor_spl::token::ID,
-            associated_token_program: associated_token::ID,
-            system_program: system_program::ID,
-        })
-        .args(bridge_escrow::instruction::EscrowAndStoreIntent { new_intent })
-        .payer(src_user.clone())
-        .signer(&*src_user)
-        .send_with_spinner_and_config(RpcSendTransactionConfig {
-            skip_preflight: true,
-            ..Default::default()
-        });
+    loop {
+        let sig = program
+            .request()
+            .accounts(bridge_escrow::accounts::EscrowAndStoreIntent {
+                user: src_user.pubkey(),
+                user_token_account: user_token_in_addr,
+                auctioneer_state,
+                token_mint: token_in,
+                escrow_token_account: token_in_escrow_addr,
+                intent: intent_state,
+                token_program: anchor_spl::token::ID,
+                associated_token_program: associated_token::ID,
+                system_program: system_program::ID,
+            })
+            .args(bridge_escrow::instruction::EscrowAndStoreIntent { new_intent: new_intent.clone() })
+            .payer(src_user.clone())
+            .signer(&*src_user)
+            .send_with_spinner_and_config(RpcSendTransactionConfig {
+                skip_preflight: true,
+                ..Default::default()
+            });
 
-    match sig {
-        Ok(signature) => Ok(signature),
-        Err(err) => Err(format!("Transaction failed: {}", err)),
+        match sig {
+            Ok(signature) => break Ok(signature), // Transaction succeeded, exit loop
+            Err(err) if err.to_string().contains("unable to confirm transaction") => {
+                eprintln!("Transaction failed: {}. Retrying...", err);
+                let _= sleep(Duration::from_secs(1));
+            },
+            Err(err) => break Err(format!("Transaction failed due to a non-retryable error: {}", err)), // Break on other errors
+        }
     }
 }
 
