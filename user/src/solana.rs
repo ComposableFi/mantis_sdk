37618666c4566fe4104396fc4f5625b5113a9462
doradelta <64730977+doradelta@users.noreply.github.com>
2024-11-20 18:16:41 +0100

    Update solana.rs

--- user/src/solana.rs
+++ user/src/solana.rs
@@ -8,7 +8,6 @@ use anchor_spl::associated_token;
 use anchor_spl::associated_token::get_associated_token_address;
 use anyhow::Result;
 use solana_client::rpc_config::RpcSendTransactionConfig;
-use solana_sdk::compute_budget::ComputeBudgetInstruction;
 use solana_sdk::pubkey;
 use solana_sdk::signature::Signature;
 use solana_sdk::system_instruction;
@@ -31,7 +30,7 @@ pub fn escrow_and_store_intent_solana(
     client: &Client<Rc<Keypair>>,
     intent_id: String,
     amount_in: u64,
-    token_in: Pubkey,
+    mut token_in: Pubkey,
     dst_user: Pubkey,
     token_out: String,
     amount_out: String,
@@ -42,6 +41,11 @@ pub fn escrow_and_store_intent_solana(
         Ok(prog) => prog,
         Err(err) => return Err(format!("Failed to get program: {}", err)),
     };
+
+    if token_in == Pubkey::from_str("11111111111111111111111111111111").unwrap() {
+        token_in = Pubkey::from_str("So11111111111111111111111111111111111111112").unwrap();
+        let _ = ensure_wsol_balance_blocking(src_user, amount_in).unwrap();
+    }
 
     let user_token_in_addr = get_associated_token_address(&src_user.pubkey(), &token_in);
     let token_in_escrow_addr = get_associated_token_address(&auctioneer_state, &token_in);
@@ -67,10 +71,6 @@ pub fn escrow_and_store_intent_solana(
         timeout_timestamp_in_sec: current_timestamp + timeout_duration,
         single_domain: single_domain,
     };
-
-    if token_in == Pubkey::from_str("11111111111111111111111111111111").unwrap() {
-        let _ = ensure_wsol_balance_blocking(src_user, amount_in).unwrap();
-    }
 
     loop {
         let sig = program
@@ -118,7 +118,7 @@ pub fn escrow_and_store_intent_cross_chain_solana(
     client: &Client<Rc<Keypair>>,
     intent_id: String,
     amount_in: u64,
-    token_in: Pubkey,
+    mut token_in: Pubkey,
     dst_user: String,
     token_out: String,
     amount_out: String,
@@ -129,6 +129,11 @@ pub fn escrow_and_store_intent_cross_chain_solana(
         Ok(prog) => prog,
         Err(err) => return Err(format!("Failed to get program: {}", err)),
     };
+
+    if token_in == Pubkey::from_str("11111111111111111111111111111111").unwrap() {
+        token_in = Pubkey::from_str("So11111111111111111111111111111111111111112").unwrap();
+        let _ = ensure_wsol_balance_blocking(src_user, amount_in).unwrap();
+    }
 
     let user_token_in_addr = get_associated_token_address(&src_user.pubkey(), &token_in);
     let token_in_escrow_addr = get_associated_token_address(&auctioneer_state, &token_in);
@@ -154,10 +159,6 @@ pub fn escrow_and_store_intent_cross_chain_solana(
         timeout_timestamp_in_sec: current_timestamp + timeout_duration,
         single_domain: single_domain,
     };
-
-    if token_in == Pubkey::from_str("11111111111111111111111111111111").unwrap() {
-        let _ = ensure_wsol_balance_blocking(src_user, amount_in).unwrap();
-    }
 
     loop {
         let sig = program
@@ -199,10 +200,7 @@ pub fn escrow_and_store_intent_cross_chain_solana(
     }
 }
 
-pub fn ensure_wsol_balance_blocking(
-    fee_payer: &Rc<Keypair>,
-    amount_in: u64,
-) -> Result<(), String> {
+pub fn ensure_wsol_balance_blocking(fee_payer: &Rc<Keypair>, amount_in: u64) -> Result<(), String> {
     let rpc_url = env::var("SOLANA_RPC").expect("SOLANA_RPC must be set");
     let rpc_client = solana_client::rpc_client::RpcClient::new_with_commitment(
         rpc_url.clone(),
@@ -274,63 +272,62 @@ pub fn ensure_wsol_balance_blocking(
     }
 }
 
+// fn _user_cancel_intent_solana(
+//     wallet: Rc<Keypair>,
+//     auctioneer_state: Pubkey,
+//     client: Client<Rc<Keypair>>,
+//     intent_id: String,
+// ) {
+//     // Derive necessary accounts
+//     let user = wallet.pubkey();
+//     let user_token_in = get_associated_token_address(
+//         &user,
+//         &Pubkey::from_str("Es9vMFrzaCERmJfrF4H2FYD4KCoNkY11McCe8BenwNYB").unwrap(),
+//     );
+//     let auctioneer_token_in = get_associated_token_address(
+//         &auctioneer_state,
+//         &Pubkey::from_str("Es9vMFrzaCERmJfrF4H2FYD4KCoNkY11McCe8BenwNYB").unwrap(),
+//     );
 
-fn _user_cancel_intent_solana(
-    wallet: Rc<Keypair>,
-    auctioneer_state: Pubkey,
-    client: Client<Rc<Keypair>>,
-    intent_id: String,
-) {
-    // Derive necessary accounts
-    let user = wallet.pubkey();
-    let user_token_in = get_associated_token_address(
-        &user,
-        &Pubkey::from_str("Es9vMFrzaCERmJfrF4H2FYD4KCoNkY11McCe8BenwNYB").unwrap(),
-    );
-    let auctioneer_token_in = get_associated_token_address(
-        &auctioneer_state,
-        &Pubkey::from_str("Es9vMFrzaCERmJfrF4H2FYD4KCoNkY11McCe8BenwNYB").unwrap(),
-    );
+//     // Call the on_timeout method to cancel intent
+//     let program = client.program(bridge_escrow::ID);
+//     let auctioneer = Pubkey::from_str("5zCZ3jk8EZnJyG7fhDqD6tmqiYTLZjik5HUpGMnHrZfC").unwrap();
 
-    // Call the on_timeout method to cancel intent
-    let program = client.program(bridge_escrow::ID);
-    let auctioneer = Pubkey::from_str("5zCZ3jk8EZnJyG7fhDqD6tmqiYTLZjik5HUpGMnHrZfC").unwrap();
-
-    let _sig = program
-        .unwrap()
-        .request()
-        .instruction(ComputeBudgetInstruction::set_compute_unit_limit(1_000_000)) // Optional: Increase compute units if needed
-        .accounts(bridge_escrow::accounts::OnTimeout {
-            user: wallet.pubkey(),
-            auctioneer_state,
-            auctioneer: auctioneer,
-            intent: Some(
-                Pubkey::find_program_address(
-                    &[b"intent", intent_id.as_bytes()],
-                    &bridge_escrow::ID,
-                )
-                .0,
-            ),
-            token_in: Some(
-                Pubkey::from_str("Es9vMFrzaCERmJfrF4H2FYD4KCoNkY11McCe8BenwNYB").unwrap(),
-            ), // Modify if cross-chain
-            user_token_account: Some(user_token_in),
-            escrow_token_account: Some(auctioneer_token_in),
-            token_program: anchor_spl::token::ID,
-            associated_token_program: anchor_spl::associated_token::ID,
-            system_program: solana_program::system_program::ID,
-        })
-        .args(bridge_escrow::instruction::UserCancelIntent {
-            intent_id: intent_id.clone(),
-        })
-        .payer(wallet.clone())
-        .signer(&*wallet)
-        .send_with_spinner_and_config(RpcSendTransactionConfig {
-            skip_preflight: true,
-            ..Default::default()
-        })
-        .unwrap();
-}
+//     let _sig = program
+//         .unwrap()
+//         .request()
+//         .instruction(ComputeBudgetInstruction::set_compute_unit_limit(1_000_000)) // Optional: Increase compute units if needed
+//         .accounts(bridge_escrow::accounts::OnTimeout {
+//             user: wallet.pubkey(),
+//             auctioneer_state,
+//             auctioneer: auctioneer,
+//             intent: Some(
+//                 Pubkey::find_program_address(
+//                     &[b"intent", intent_id.as_bytes()],
+//                     &bridge_escrow::ID,
+//                 )
+//                 .0,
+//             ),
+//             token_in: Some(
+//                 Pubkey::from_str("Es9vMFrzaCERmJfrF4H2FYD4KCoNkY11McCe8BenwNYB").unwrap(),
+//             ), // Modify if cross-chain
+//             user_token_account: Some(user_token_in),
+//             escrow_token_account: Some(auctioneer_token_in),
+//             token_program: anchor_spl::token::ID,
+//             associated_token_program: anchor_spl::associated_token::ID,
+//             system_program: solana_program::system_program::ID,
+//         })
+//         .args(bridge_escrow::instruction::UserCancelIntent {
+//             intent_id: intent_id.clone(),
+//         })
+//         .payer(wallet.clone())
+//         .signer(&*wallet)
+//         .send_with_spinner_and_config(RpcSendTransactionConfig {
+//             skip_preflight: true,
+//             ..Default::default()
+//         })
+//         .unwrap();
+// }
 
 pub async fn _create_token_account(
     owner: &Pubkey,
